c780d1eba91c4fceef8b3918c2aec4698186efdd
Xuanxuan Yuan <www.420426962.com@qq.com>
2017-01-30 13:59:12 +0000

    User microposting feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+
 	 attr_accessor :remember_token       # NEW LINE
+
+
+	has_many :microposts, dependent: :destroy
+
      before_save { self.email = email.downcase }
      validates :name, presence: true, length: { in: 9..30 }
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,6 +13,7 @@ class User < ApplicationRecord
                         uniqueness: { case_sensitive: false }
       validates :password, presence: true, length: { minimum: 6 }
       has_secure_password
+
 
       
        # ALL NEW METHODS
@@ -35,4 +41,10 @@ class User < ApplicationRecord
            def forget
               update_attribute(:remember_digest, nil)
            end  
+
+       def feed
+        Micropost.where("user_id = ?", id)
+      end  
+
+
 end
